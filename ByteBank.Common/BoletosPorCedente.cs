31a37ba2a1124f2db2f504e131dfd391702b0a44
Lucas da Cunha <lucas.cunha001@ailos.coop.br>
2024-10-14 20:22:57 -0300

    Customizando Attributes

--- ByteBank.Common/BoletosPorCedente.cs
+++ ByteBank.Common/BoletosPorCedente.cs
@@ -2,11 +2,17 @@
 {
     public class BoletosPorCedente
     {
+        [NomeColuna("Nome")]
         public string CedenteNome { get; set; }
+        [NomeColuna("CpfCnpj")]
         public string CedenteCpfCnpj { get; set; }
+        [NomeColuna("Agencia")]
         public string CedenteAgencia { get; set; }
+        [NomeColuna("Conta")]
         public string CedenteConta { get; set; }
+        [NomeColuna("Valor")]
         public decimal Valor { get; set; }
+        [NomeColuna("Quantidade")]
         public int Quantidade { get; set; }
     }
 }
---
+++ ByteBank.Common/NomeColunaAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace ByteBank.Common
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class NomeColunaAttribute : Attribute
+    {
+        public string Header { get; }
+
+        public NomeColunaAttribute(string header)
+        {
+            Header = header;
+        }
+    }
+}
